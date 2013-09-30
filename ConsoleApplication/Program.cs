f28a61bdece8b17ca94b52940e2ab6026c7d6408
David Siew <davidsiew@outlook.com>
2013-09-30 17:32:22 +0100

    - Example for CastleWindsor with NLog

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,15 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Castle.Core.Logging;
+using Castle.Facilities.Logging;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
 
 namespace ConsoleApplication
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            BootStrapContainer();
+        }
+
+        private static void BootStrapContainer()
+        {
+            using (var container = new WindsorContainer())
+            {
+                container.Install(new MyWindsorInstaller())
+                    .Resolve<IObjectGraphRoot>()
+                    .Log();
+            }
+        }
+    }
+
+    internal interface IObjectGraphRoot
+    {
+        void Log();
+    }
+
+    internal class MyWindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<LoggingFacility>(f => f.LogUsing(LoggerImplementation.NLog)
+                                                         .WithConfig("NLog.config"));
+
+            container.Register(
+                Component
+                    .For<IObjectGraphRoot>()
+                    .ImplementedBy<ObjectGraphRoot>());
+        }
+    }
+
+    internal class ObjectGraphRoot : IObjectGraphRoot
+    {
+        private ILogger _logger = NullLogger.Instance;
+
+        public ILogger Logger
+        {
+            get { return _logger; }
+            set { _logger = value; }
+        }
+
+        public void Log()
+        {
+            _logger.Info("Log something");
         }
     }
 }
